0bc6f514e40b2e4b11949eb2f456a519c75a82f9
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2017-10-24 07:37:23 -0200

    JS

---
+++ Javascript/aula1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <title>Aula 1</title>
+    <meta charset="utf-8">
+
+    <script type="text/javascript">
+        alert("Hello World");
+
+        lista = Array("Banana", "Maca", "Pera");
+        lista[4] = "OLOSCO";
+
+        alert(lista);
+        document.write(lista);
+    </script>
+</head>
+
+<body>
+
+</body>
+</html>
---
+++ Javascript/eventos.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+    <title>Eventos - Mouse</title>
+    <meta charset="utf-8">
+
+    <script type="text/javascript">
+
+        /*onclick
+          ondblclick
+          onmousedown
+          onmouseover
+          onmouseout
+
+          onkeydown
+          onkeypress
+          onkeyup
+
+          onload
+          onrezise
+
+          onfocus
+          onblur
+          onchange
+         */
+         function msgBoasVindas(){
+             alert("Pressionou o botao do mouse");
+         }
+
+    </script>
+</head>
+
+<body onresize="console.log('fsdfsd')">
+    <input type="text" onkeyup="alert('olosco')"/>
+</body>
+</html>
