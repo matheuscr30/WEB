4c9089e575b3b6486cd04795333ac2ce8595901b
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2017-10-27 10:52:36 -0200

    MyBooks PWA

---
+++ MyBooks/sw.js
@@ -0,0 +1,43 @@
+/** An empty service worker! */
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('the-magic-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/home.html',
+        '/register.html',
+        '/manifest.json',
+        '/configFirebase.js',
+        '/images/icon.png',
+        '/images/welcome_card.png',
+        '/site.js',
+        '/sw.js',
+        '/style.css',
+      ]);
+    })
+  );
+});
